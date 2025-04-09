901ab62ae750c387bc21ce0b9695d84313baf73e
Jackie-Weng-ICS4U1-3-2025 <jackie.weng@student.tdsb.on.ca>
2025-04-09 03:24:33 +0000

    Syncing

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,7 +4,7 @@ public class PracticeProblem {
         int sum = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                sum += array[i][j];
+                sum += array[i][j]; //
             }
         }
         return sum;
